ce21db08399299800ba3bd3179d6f07294c424a6
zakir7dipu <zakir67@dipti.com.bd>
2023-06-01 16:14:15 +0600

    update

--- src/assets/library/api.js
+++ src/assets/library/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import global from "./config"
 const {base_path} = global
+axios.defaults.headers['X-API-KEY'] = 'beb894742971fc790c59ac0f14502ae87c2ad09e';
 
 class Api {
+    
     apiAccess = axios.create({
         baseURL: `${base_path}api`,
         headers: {
-            "Content-type": "application/json",
-            "x-api-key": "beb894742971fc790c59ac0f14502ae87c2ad09e"
+            "Content-type": "multipart/form-data"
         }
     })
 }
